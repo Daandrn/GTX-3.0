074a8ea33fb415bfa1bc1dc2adb131e0bf26f904
Danillo Rodrigues <dandrn7@gmail.com>
2023-10-21 09:42:33 -0300

    insere versao 1 na versao 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $url = $_SERVER['REQUEST_URI'];
 
 if($url == "/inicio.php" || $url == "/index.php" || $url == "/inicio" || $url == "/"){
-    header("location: /control/control.inicio.php");
+    header("location: /gtx2/control/control.inicio.php");
 } else {
     echo "error 404";
 }
