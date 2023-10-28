1a7134b7c33bd4263f3277ee5af7201496f8ca95
Danillo Rodrigues <dandrn7@gmail.com>
2023-10-28 13:16:44 -0300

    incia area logada e adiciona versao de app

--- gtx2/view/view.areaLogada.php
+++ gtx2/view/view.areaLogada.php
@@ -29,18 +29,74 @@
             </nav>
         </div>
     </header>
-    <aside>
-        
-    </aside>
     <main class="principal">
         <h1 id="boasVindas">
         Seja bem vindo, <?php echo $boasvindas;?>!
         </h1>
+        <div id="dadosPessoais">
+            <div id="canalStream">
+                <form method="POST">
+                    <h3>Canal Stream</h3>
+                    <div>
+                        <label for="nickStream">Nick stream</label>
+                        <input type="text" name="nickStream" id="">
+                    </div>
+                    <div>
+                        <label for="linkstream">Link canal</label>
+                        <input type="text" name="linkStream" id="">
+                    </div>
+                    <div>
+                        <label for="plataforma">Plataforma</label>
+                        <select name="plataforma" id="">
+                            <option value=""></option>
+                            <option value="1">Youtube</option>
+                            <option value="2">Twitch</option>
+                            <option value="3">TikTok</option>
+                            <option value="4">Facebook</option>
+                            <option value="5">Kwai</option>
+                        </select>
+                    </div>
+                    <span id="salvarStream">
+                        <input type="hidden" name="formLogado" value="canalStream">
+                        <input type="submit" value="Salvar">
+                    </span>
+                </form>
+            </div>
+            <div id="perfil">
+                <form method="POST">
+                    <h3>Perfil</h3>
+                    <div>
+                        <label for="">Nick/origin</label>
+                        <input type="text" name="origin" id="" required>
+                    </div>
+                    <div>
+                        <label for="">Senha</label>
+                        <input type="text" name="senha" id="" placeholder="Nova senha">
+                    </div>
+                    <input type="hidden" name="formLogado" value="perfil">
+                    <input type="submit" value="Salvar" id="salvarPerfil">
+                </form>
+            </div>
+        </div>
     </main>
-    <footer>
-        <p>Ghost tóxic team&trade;</p>
-        <p>Todos os direitos reservados&copy;</p>
-        <p>2023</p>
+    <footer id="rodapeLogado">
+        <div>
+            <p>Ghost tóxic team&trade;</p>
+            <p>Todos os direitos reservados&copy;</p>
+            <p>2023</p>
+        </div>
+        <span>
+            <form method="post">
+                <label for="versao">Versao</label>
+                <select name="versao">
+                    <?php while ($resutado = $sql->fetch(PDO::FETCH_ASSOC)): ?>
+                    <option value="<?php echo $resutado['id'];?>" <?php if ($resutado['selected'] == 1) {echo "selected";} ?>><?php echo $resutado['descricao'];?></option>
+                    <?php endwhile; ?>
+                </select>
+                <input type="hidden" name="formLogado" value="salvaVersao">
+                <input type="submit" value="Salvar">
+            </form>
+        </span>
     </footer>
 </body>
 </html>
