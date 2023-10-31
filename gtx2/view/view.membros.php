a10ee8d2c9acfb2c3ecbcb720dcb99a3f4ee050c
Danillo Rodrigues <dandrn7@gmail.com>
2023-10-30 21:46:30 -0300

    adiciona tabela membros quando nao tem resultado

--- gtx2/view/view.membros.php
+++ gtx2/view/view.membros.php
@@ -28,10 +28,15 @@
             <table id="membros">
                 <caption>MEMBROS</caption>
                 <thead>
-                <tr>
-                <th style="width: 220px;">Nome</th><th style="width: 143px;">Origin</th><th style="width: 108px;">Cargo</th><th style="width: 143px;">Nick stream</th><th style="width: 78px;">Plataforma</th>
-                </tr>
+                    <tr>
+                        <th style="width: 220px;">Nome</th>
+                        <th style="width: 143px;">Origin</th>
+                        <th style="width: 108px;">Cargo</th>
+                        <th style="width: 143px;">Nick stream</th>
+                        <th style="width: 78px;">Plataforma</th>
+                    </tr>
                 </thead>
+                <?php if ($consulta->rowCount() > 0): ?>
                 <?php while ($resultado = $consulta->fetch(PDO::FETCH_ASSOC)):?>
                 <tr id="lista">
                     <td><?php echo $resultado['nome'];?></td>
@@ -41,6 +46,11 @@
                     <td><?php echo $resultado['plataforma'];?></td>
                 </tr>
                 <?php endwhile ?>
+                <?php else: ?>
+                    <tr id="lista">
+                        <td colspan="5">Nenhum membro encontrado!</td>
+                    </tr>
+                <?php endif; ?>
             </table>
         </div>
     </main>
