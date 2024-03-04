6f8d0af1083b64f1bf6b284cfe52928ca4d3b3d0
daandrn <dandrn7@gmail.com>
2024-03-03 21:45:33 -0300

    wip: atualiza conn

--- Config/Connection.php
+++ Config/Connection.php
@@ -11,7 +11,7 @@ function connection(): PDO|string
     $host     = "localhost";
     $dataBase = "GTX3";
     $user     = "postgres";
-    $password = "1234";
+    $password = "5";
     
     try {
         $conn = new PDO("$drive:host=$host;dbname=$dataBase", $user, $password);
