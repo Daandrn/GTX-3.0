aad93ca632f57d2e62a11e64543bdc6535f245bb
daandrn <dandrn7@gmail.com>
2024-04-30 20:26:33 -0300

    wip: adiciona novas rotas

--- Routes/web.php
+++ Routes/web.php
@@ -10,8 +10,11 @@ use Vendor\Routes\Route;
 
 require __DIR__.'/../Vendor/Routes/Route.php';
 
-Route::post(['login'], InicioController::class, 'inicioLogin');
 Route::get(['inicio', ''], InicioController::class, 'index');
-Route::get(['arealogada', ''], AreaLogadaController::class, 'index');
+Route::post(['login'], InicioController::class, 'inicioLogin');
+Route::delete(['excluir'], MembrosController::class, 'delete');
+Route::patch(['alterastatusmembro'], MembrosController::class, 'alteraStatusMembro');
+Route::post(['novo'], InicioController::class, 'inicioRecruit');
+Route::get(['arealogada'], AreaLogadaController::class, 'index');
 Route::get(['salavideos'], SalaVideosController::class, 'index');
 Route::get(['membros'], MembrosController::class, 'listaMembros');
