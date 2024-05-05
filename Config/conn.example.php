051cd2448dc75e02c5894470d896aea369c8fffb
daandrn <dandrn7@gmail.com>
2024-05-05 17:49:05 -0300

    conexao exmplo

---
+++ Config/conn.example.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Config;
+
+use Exception;
+use PDO;
+
+function connection(): PDO|string
+{
+    $drive    = "pgsql";
+    $host     = "localhost";
+    $dataBase = "GTX3";
+    $user     = "postgres";
+    $password = "";
+    
+    try {
+        $conn = new PDO("$drive:host=$host;dbname=$dataBase", $user, $password);
+
+        return $conn;
+    } catch (\Throwable $error) {
+        throw new Exception("Erro ao conectar ao banco de dados: " . $error->getMessage());
+    }
+}
