e1a967c4799633a0daae9009765a46c7dfcda93e
daandrn <dandrn7@gmail.com>
2024-05-05 21:14:51 -0300

    wip: inserindo autoload com composer

---
+++ Vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daandrn/gtx-3.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '34e483c50e33d2b13417ef30d45d2b92dde699c3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daandrn/gtx-3.0' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '34e483c50e33d2b13417ef30d45d2b92dde699c3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
