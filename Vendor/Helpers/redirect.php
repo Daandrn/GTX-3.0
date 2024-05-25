eb33ed6a9b8d4f61a0563cc5bb86b460f796baf2
daandrn <dandrn7@gmail.com>
2024-05-25 20:59:11 -0300

    fix: ajusta função redirect

--- Vendor/Helpers/redirect.php
+++ Vendor/Helpers/redirect.php
@@ -7,31 +7,34 @@ use Vendor\Routes\Route;
 
 require_once __DIR__.'/../autoload.php';
 
-/**
- * Realiza redireciamento para as rotas definidas.
- * @param string $classMethod Deve receber a classe e methodo para onde será redirecionado no padrão "Class.method".
- */
-function redirect(?string $route = null, ?string $classMethod = null, ?array $errors = null, ?array $data = null)
-{   
-    if ($route) {
-        header("Location: /{$route}");
-        return;
-    }
+class Redirect
+{
+    /**
+     * Realiza redireciamento para as rotas definidas.
+     * @param string $classMethod Deve receber a classe e methodo para onde será redirecionado no padrão "Class.method".
+     */
+    public static function to(?string $route = null, ?string $classMethod = null, ?array $errors = null, ?array $data = null)
+    {   
+        if ($route) {
+            header("Location: /{$route}");
+            return;
+        }
 
-    if (
-        !$classMethod
-        || !preg_match('/\A\w{1,}\.\w{1,}\z/', $classMethod)
-        || strlen($classMethod) < 7
-    ) {
-        throw new Exception("Classe ou methodo inválidos: {$classMethod}");
-    }
+        if (
+            !$classMethod
+            || !preg_match('/\A\w{1,}\.\w{1,}\z/', $classMethod)
+            || strlen($classMethod) < 7
+        ) {
+            throw new Exception("Classe ou methodo inválidos: {$classMethod}");
+        }
 
-    $classMethod = explode('.', $classMethod);
+        $classMethod = explode('.', $classMethod);
 
-    $class  = $classMethod[0];
-    $method = $classMethod[1];
+        $class  = $classMethod[0];
+        $method = $classMethod[1];
 
-    if ($class && $method) {    
-        return Route::redirection("$class", $method, $errors, $data);
+        if ($class && $method) {    
+            return Route::redirection("$class", $method, $errors, $data);
+        }
     }
 }
