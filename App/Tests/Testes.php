70d430270df7a351e3672f6f5f8f9032650af28d
daandrn <dandrn7@gmail.com>
2024-06-01 23:18:34 -0300

    wipe: implementando testes.

--- App/Tests/Testes.php
+++ App/Tests/Testes.php
@@ -3,18 +3,46 @@
 namespace App\tests;
 
 use App\Repositories\StreamChannelRepository;
+use App\Requests\Request;
 
 require_once __DIR__ . '/../../Vendor/autoload.php';
 
 class Testes
 {
-    public function teste()
+    /**
+     * Chama os metodos que contém os cenários de testes criados.
+     */
+    public function init()
     {
-        $this->novoCanalStream();
+        //$this->novoCanalStream(999);
+        //$this->verificaConteudoRequisicao();
+        //$this->verificaSessao();
     }
 
-    private function novoCanalStream()
+    private function novoCanalStream(int $id)
     {
-        (new StreamChannelRepository())->newStream(999);
+        (new StreamChannelRepository())->new($id);
+    }
+
+    private function verificaConteudoRequisicao()
+    {
+        var_dump(Request::new());
+        echo "<br>";
+        var_dump(Request::toArray());
+        echo "<br>";
+    }
+
+    private function verificaSessao()
+    {
+        session_start();
+        var_dump($_SESSION);
+        echo "<br>";
+    }
+
+    public static function run()
+    {
+        $tests = new Testes;
+        $tests->init();
+        exit;
     }
 }
