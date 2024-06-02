047c693e0cab4eb741d496fbd203593feb337e8d
daandrn <dandrn7@gmail.com>
2024-06-01 23:18:58 -0300

    feat: cria função para sanitizar entrada de input.

---
+++ Vendor/Helpers/sanitizeInput.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Vendor\Helpers;
+
+use Exception;
+
+require_once __DIR__.'/../autoload.php';
+
+class SanitizeInput
+{
+    public static function make(mixed $input): mixed
+    {
+        if (is_string($input)) {
+            $input = trim($input);
+            $input = htmlspecialchars($input, ENT_QUOTES | ENT_HTML5, encoding: 'UTF-8');
+            $input = str_replace("\0", "", $input);
+            $input = preg_replace("/\s\s+/", " ", $input);
+    
+            return $input;
+        }
+
+        if (is_int($input)) {
+            return $input;
+        }
+
+        if (is_array($input)) {
+            foreach ($input as &$item) {
+                $item = self::make($item);
+            }
+
+            return $input;
+        }
+
+        throw new Exception("Os dados inseridos no campo é inválido: " . print_r($input), 500);
+
+    }
+}
